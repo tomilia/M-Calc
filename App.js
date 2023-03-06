48a01277fff1a1dad88cf8e8aa1d712bae26a265
tomibo0402 <98295588+tomibo0402@users.noreply.github.com>
2023-03-07 02:03:45 +0800

    - init commit with calendar

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
 export default function App() {
+  const [mDateNum, setMDateNum] = useState("0");
+  const mComingColor = {key:'mcome', color:'red'};
+  const marked = {
+    '2023-02-15': {"color": "rbga(255, 255, 255, .30)", "dotColor": "#E892F8", "marked": true},
+    '2023-02-11': {"color": "rbga(255, 255, 255, .30)", "dotColor": "#E892F8", "marked": true},
+  };
+  const [selectedDate, setSelectedDate] = useState('');
+  onChanged = (text) =>
+  {
+    setMDateNum(text)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ justifyContent: 'center',flex: 1, alignItems: 'center' }}>
+      <TextInput
+        style={styles.input}
+        keyboardType='numeric'
+        underlineColorAndroid="red"
+        onChangeText={(text) => this.onChanged(text)}
+        value={mDateNum}
+        placeholder="Enter text"
+        placeholderTextColor="#ccc"
+        onDayPress={(day) => setSelectedDate(day.dateString)}
+        borderWidth={1}
+        borderColor="#ccc"
+        borderRadius={4}
+        markingType="multi-dot"
+        markedDates={{marked}}
+      />
+      <View style={{ marginTop: 16 }}>
+        <Calendar/>
+      </View>
     </View>
   );
 }
@@ -16,5 +46,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    fontSize: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+    color: '#333',
+  },
+  underline: {
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
   },
 });
